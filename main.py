7d0aed0377f82a8ac30d51d786f425975f1fdb1d
KMDEV99 <matkonrad99@gmail.com>
2020-11-10 21:53:02 +0100

    Fixed handling arguments

--- main.py
+++ main.py
@@ -4,10 +4,11 @@ import click
 from classification_dict import classification_dict
 from intent_getter import create_regex, get_intent
 
+
 @click.command()
-@click.option('--sentence', help='Sentence to get intent from(str).')
-#@click.option('--path', help='Path to .txt file containing sentence to get intent from(str).')
-def main(sentence):
+@click.option('--sentence', '-s', help='Sentence to get intent from(str).', default="")
+@click.option('--path', '-p', help='Path to .txt file containing sentence to get intent from(str).', default="")
+def main(sentence, path):
     classification_regex = create_regex(classification_dict.keys())
     print(get_intent(sentence, classification_regex))
 
